ace2e4e2e31e42522e78bbfea89cac05f1728b62
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-05 09:49:55 +0000

    Repurposing project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import {useSelector, useDispatch} from 'react-redux';
 
 function App() {
-const counter = useSelector((state) => state.counter);
-const dispatch = useDispatch();
-
-const increment =() => {
-  dispatch({type:"INC"});
-}
-
-const decrement =() => {
-  dispatch({type:"DEC"});
-}
-return (
-  <div>
-  <div>Counter</div>
-  <div>{counter}</div>
-  <button onClick={increment}>Increment</button>
-  <button onClick={decrement}>Decrement</button>
-  </div>
-)
+  return(
+    <div></div>
+  );
 
 }
 
